9cb2d4c5943841ab2096f62d5426f4ed973d4312
Kai.W <z1191010@gmail.com>
2023-03-01 11:15:21 +0800

    Initial Commit

---
+++ LotteryTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LotteryTicket
+//
+//  Created by Kai on 2023/3/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
